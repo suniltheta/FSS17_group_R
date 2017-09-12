8607733c4069d768507af7faab8101d30c4d1750
Raghav <rmuddur@gmail.com>
2017-09-12 17:40:45 -0400

    Workshop changes
    Workshop changes

---
+++ workshop/rmuddur.py
@@ -0,0 +1,71 @@
+# KNN Visualization.
+# author : Raghav Muddur
+#n_neighbors = 5
+h = .02  # step size in the mesh
+
+# Create color maps
+from matplotlib.colors import ListedColormap
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+for n_neighbors in [5,10,15,20]:
+    for weights in ['uniform', 'distance']:
+        # we create an instance of Neighbours Classifier and fit the data.
+        knn = KNeighborsClassifier(n_neighbors, weights=weights)
+        knn.fit(X, y)
+
+        # Plot the decision boundary. For that, we will assign a color to each
+        # point in the mesh [x_min, x_max]x[y_min, y_max].
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                             np.arange(y_min, y_max, h))
+        Z = knn.predict(np.c_[xx.ravel(), yy.ravel()])
+
+        # Put the result into a color plot
+        Z = Z.reshape(xx.shape)
+        plt.figure()
+        plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+        # Plot also the training points
+        plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
+                    edgecolor='k', s=20)
+        plt.xlim(xx.min(), xx.max())
+        plt.ylim(yy.min(), yy.max())
+        plt.title("3-Class classification (k = %i, weights = '%s')"
+                  % (n_neighbors, weights))
+
+plt.show()
+
+
+""" Part 3 """
+from sklearn import svm
+svc = svm.SVC(kernel='rbf')
+svc.fit(iris_X_train, iris_y_train)  
+svc.predict(iris_X_test)
+print iris_y_test
+
+
+
+for kernel in ['linear', 'rbf']:
+    # we create an instance of Neighbours Classifier and fit the data.
+    svc = svm.SVC(kernel=kernel)
+    svc.fit(X, y)
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = svc.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
+                edgecolor='k', s=20)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.title("3-Class classification (kernel = '%s')"
+              % (kernel))
+
+plt.show()
