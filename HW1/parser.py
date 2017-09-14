58cb748bed32744d53b2451ef19f3cdcd0636b02
Sunil Narasimhamurthy <suniltheta@gmail.com>
2017-09-13 21:20:40 -0400

    Update after test case given by TA
    Now prints:
    Error logs
    Total time taken to execute
    All test cases handled

--- HW1/parser.py
+++ HW1/parser.py
@@ -1,22 +1,34 @@
 import sys
 import os.path
+from tbl import Tbl
+import time
 
 first_row = []
+first_row_type = []
 all_rows = []
+bad_row = []
 result = {}
 
 def tokenize_first_row(list = []):
     for item in list:
         item = item.strip()
         if not item[0] == '?':
+            if item[0] == '$' or item[0] == '%' or item[0] == '>' or item[0] == '<':
+                first_row_type.append(1.0)
+            elif item[0] == '!':
+                first_row_type.append("str")
+            else:
+                first_row_type.append("str")
             first_row.append(item)
             result[item] = []
         else:
-            first_row.append(False)
+            # first_row.append(False)
+            first_row_type.append(False)
 
 
 def tokenize_row(list = []):
     skips = 0
+    temp_result = []
     for i, item in enumerate(list):
         item = item.strip()
         if item == '':
@@ -25,6 +37,9 @@ def tokenize_row(list = []):
         try:
             val = float(item)
         except ValueError:
+            if first_row_type[i] == 1.0:
+                bad_row.append(list)
+                return
             if item == 'True' or item == 'true' or item == 'TRUE' or item == 't' or item == 'T':
                 val = True
             elif item == 'False' or item == 'false' or item == 'FALSE' or item == 'f' or item == 'F':
@@ -32,8 +47,14 @@ def tokenize_row(list = []):
             else:
                 val = item
         index = i - skips
-        if first_row[index] and index < len(first_row):
-            result[first_row[index]].append(val)
+        # if first_row[index] and index < len(first_row):
+        #     result[first_row[index]].append(val)
+        if first_row_type[index] and index < len(first_row_type):
+            # result[first_row[index]].append(val)
+            temp_result.append(val)
+
+    for i in range(len(temp_result)):
+        result[first_row[i]].append(temp_result[i])
 
 
 def main(file_name):
@@ -53,13 +74,18 @@ def main(file_name):
             all_rows.append(line.split('#')[0])
 
     weird_rows = {}
+    skip_next = False
     l = len(all_rows)
 
     for index, row in enumerate(all_rows):
         row = row.strip()
+        if skip_next:
+            skip_next = False
+            continue
         if row == '' or len(row) == 0 :
             continue
         if row[-1] == ',':
+            skip_next = True
             if index < (l - 1):
                 weird_rows[index] = {"current": row, "next": all_rows[index + 1]}
         else:
@@ -72,7 +98,7 @@ def main(file_name):
             tokenize_row(item.get("current").split(',') + item.get("next").split(','))
         prev_key = key
 
-    # print(result)
+    temp_result = result
     for key in result.keys():
         print("{}:\t{}\n".format(key, result.get(key)))
         
@@ -80,9 +106,19 @@ def main(file_name):
 
 if __name__ == "__main__":
     #main("test.csv")
+    start_time = time.time()
+    time.sleep(0.1)
     if len(sys.argv) > 1:
          main(sys.argv[1])
     else:
          print("Please enter the .csv file name")
+    print("\n\n$$$$$$$$$$$$ printing time in seconds $$$$$$$$$$$$")
+    print("--- {} seconds ---".format(time.time() - start_time - 0.1))
+    print("$$$$$$$$$$$$ time in seconds $$$$$$$$$$$$\n\n")
+    print("\n\n$$$$$$$$$$$$ printing error log $$$$$$$$$$$$")
+    print("Found {} number of error lines".format(len(bad_row)))
+    for row in bad_row:
+        print("{}".format(row))
+    print("$$$$$$$$$$$$ printing error log $$$$$$$$$$$$\n\n")
 
 
